3dd9f64c00de9d8516afe877ce586f23e616ef82
KangSeokJeon <studyandinvestigate@gmail.com>
2022-10-19 01:48:00 +0900

    chap4-exercise

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import Customer from "./components/Customer";
+
+const customer = {
+  name: "홍길동",
+  birthday: "020803",
+  gender: "남자",
+  job: "대학생",
+};
 
 function App() {
-  let [title, setTitle] = useState([
-    "남자 코트 추천",
-    "강남 우동 맛집",
-    "파이썬 독학",
-  ]);
-  let [numLike, setNumLike] = useState(0);
-  let logo = "ReactBlog";
-
   return (
     <div className="App">
-      <div className="black-nav">
-        <h4>{logo}</h4>
-      </div>
-      <div className="list">
-        <h4>
-          {title[0]}{" "}
-          <span
-            onClick={() => {
-              setNumLike(numLike + 1);
-            }}
-          >
-            👍
-          </span>{" "}
-          {numLike}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <Modal />
-
-      <button
-        onClick={() => {
-          let copy = [...title];
-          copy.sort();
-          setTitle(copy);
-        }}
-      >
-        가나다순 정렬
-      </button>
-    </div>
-  );
-}
-
-function Modal() {
-  return (
-    <div className="modal">
-      <h4>제목</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     </div>
   );
 }
