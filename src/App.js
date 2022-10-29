3d832f84c89b7f8192e904dcf26cd87c993cf9b0
KangSeokJeon <studyandinvestigate@gmail.com>
2022-10-30 01:33:49 +0900

    chap5-exercise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "홍길동",
-  birthday: "020803",
-  gender: "남자",
-  job: "대학생",
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "991004",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "전강석",
+    birthday: "020803",
+    gender: "남자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "안준혁",
+    birthday: "020316",
+    gender: "남자",
+    job: "디자이너",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
